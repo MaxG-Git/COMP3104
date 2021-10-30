af071b4f4926ab4b63f298de476007054ceb4dec
maxG-git <maxpgrossman@gmail.com>
2021-10-30 12:53:54 -0400

    Changes to travis.yml

---
+++ build/precache-manifest.655d440ce73d7c957c1f14f41ff7d3be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a30bc40f37d590427a",
+    "url": "/MaxG-Git/COMP3104/static/css/main.bc7e3f79.chunk.css"
+  },
+  {
+    "revision": "16a30bc40f37d590427a",
+    "url": "/MaxG-Git/COMP3104/static/js/main.16a30bc4.chunk.js"
+  },
+  {
+    "revision": "d67dea921c0568d4a251",
+    "url": "/MaxG-Git/COMP3104/static/js/runtime~main.d67dea92.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/MaxG-Git/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "39a0d99467514dcf69f89306fd2bf5ea",
+    "url": "/MaxG-Git/COMP3104/index.html"
+  }
+];

610421d6cdca59cf12464fd75ef68fe6d72f8348
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-30 18:25:49 +0000

    Deploy MaxG-Git/COMP3104 to github.com/MaxG-Git/COMP3104.git:gh-pages

---
+++ precache-manifest.944e66e77d327421898fba5dd344627a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b07f343cc2028c936a70",
+    "url": "/MaxG-Git/COMP3104/static/css/main.2ed5412f.chunk.css"
+  },
+  {
+    "revision": "b07f343cc2028c936a70",
+    "url": "/MaxG-Git/COMP3104/static/js/main.b07f343c.chunk.js"
+  },
+  {
+    "revision": "d67dea921c0568d4a251",
+    "url": "/MaxG-Git/COMP3104/static/js/runtime~main.d67dea92.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/MaxG-Git/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "44bf208e2f61f1c1fd439eef7c7c5125",
+    "url": "/MaxG-Git/COMP3104/index.html"
+  }
+];
